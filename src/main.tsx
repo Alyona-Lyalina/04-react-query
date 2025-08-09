0fca0978fec4195c95ac7fa728828113c02bd0b2
Alyona Lyalina <lyalina.alyona@icloud.com>
2025-08-09 15:33:12 +0300

    create basic structure

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "modern-normalize/modern-normalize.css";
+// import { App } from "./components/App/App";
+
+
+// Create a React root and render the App inside the element with id="root"
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    {/* <App /> */}
+  </StrictMode>
+);
