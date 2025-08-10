3fc2c156668effa3d2da80741b6b6c898d77d8fd
Alyona Lyalina <lyalina.alyona@icloud.com>
2025-08-10 14:59:12 +0300

    Add TMDB API keys in .env and install axios

---
+++ src/components/services/moviesApi.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import type { Movie } from "../types/movie"; 
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
+
+export async function fetchMovies(query: string): Promise<Movie[]> {
+  const response = await api.get<{ results: Movie[] }>("search/movie", {
+    params: {
+      api_key: import.meta.env.VITE_TMDB_API_KEY,
+      query,
+      include_adult: false,
+      language: "en-US",
+      page: 1,
+    },
+  });
+
+  return response.data.results;
+}
