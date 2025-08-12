a8cda7b39a6ad63d7901052a8b04a89deca68aaf
Alyona Lyalina <lyalina.alyona@icloud.com>
2025-08-12 14:57:03 +0300

    add loader

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useState } from 'react';
 import styles from './App.module.css';
-import SearchBar from '../SearchBar/SearchBar'
-import { fetchMovies } from "../services/moviesApi";
-import type { Movie } from "../types/movie";
-import MovieGrid from "../MovieGrid/MovieGrid";
+import SearchBar from '../SearchBar/SearchBar';
+import { fetchMovies } from '../services/moviesApi';
+import type { Movie } from '../types/movie';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import Loader from '../Loader/Loader';
 
 export default function App() {
-      const [movies, setMovies] = useState<Movie[]>([]);
-      const handleSubmit = async (value: string) => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [load, setLoad] = useState(false)
+  const handleSubmit = async (value: string) => {
+        setLoad(true)
         const data = await fetchMovies(value);    
-          
         setMovies(data);
+        setLoad(false)
       };
 
   return (
@@ -18,8 +21,8 @@ export default function App() {
           <div className={styles.content}>
             <SearchBar handleSubmit={handleSubmit} />
       </div>
-      
-      <MovieGrid movies={movies} onSelect={() => {}} />
+      {load && <Loader />}
+      {!load && <MovieGrid movies={movies} onSelect={() => {}} />}
     </div>
   );
 }
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,15 +1,16 @@
-import { ClipLoader } from "react-spinners";
+import { RingLoader } from "react-spinners";
 import styles from "./Loader.module.css";
 
 export default function Loader({ loading = true }) {
   return (
     <div className={styles.loaderWrapper}>
-      <ClipLoader
-        color="#00BFFF"
-        loading={loading}
-        size={80}
-        aria-label="Loading Spinner"
-      />
+      <RingLoader
+            color="#1620aeff"
+            loading={loading}
+            size={80}
+            speedMultiplier={1} 
+          />
+      {loading && <p className={styles.text}>Loading movies, please wait...</p>}
     </div>
   );
 }
