c06acdf84f937b4bba797c8160ada5341dbfdbef
Alyona Lyalina <lyalina.alyona@icloud.com>
2025-08-14 14:29:22 +0300

    Add new libraries for React Query and Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'modern-normalize/modern-normalize.css';
-import App  from './components/App/App';
+import App from './components/App/App';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"; 
 
+const queryClient = new QueryClient();
 
 // Create a React root and render the App inside the element with id="root"
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
